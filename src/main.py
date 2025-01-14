26ae92ab1cb7f170468d745515b1b603f4125864
megamaz <raphael.mazuel@gmail.com>
2025-01-14 12:11:03 -0800

    implemented manual wiggles

--- src/main.py
+++ src/main.py
@@ -7,17 +7,8 @@ import simulator
 import clipboard
 import pygubu
 
-def accept_multiline_input(terminator:str="DONE") -> str:
-    all_lines = []
-    while True:
-        data = input("... ")
-        if data != terminator:
-            all_lines.append(data)
-        else:
-            break
-    return "\n".join(all_lines)
 
-def method_normal_pullup(initial_angle, initial_speed, facing, frames):
+def method_normal_pullup(initial_angle: float, initial_speed: float, facing: Facings, frames: int) -> list[float]:
     c_angle, c_speed = initial_angle, initial_speed
     frame_data = []
     for _ in range(frames):
@@ -30,14 +21,51 @@ def method_normal_pullup(initial_angle, initial_speed, facing, frames):
 
         c_angle, c_speed = simulator.simulate(c_angle, c_speed, angle)
         frame_data.append(angle)
-    
+
     return frame_data
+
 
 def method_megajoule(initial_angle, initial_speed, facing, frames, target):
     ...
 
-def method_manual_wiggle(initial_angle, initial_speed, facing, frames, wiggle_horizontal, wiggle_vertical):
-    ...
+
+def method_manual_wiggle(initial_angle: float, initial_speed: float, facing: Facings, frames: int, wiggle_horizontal: int, wiggle_vertical: int, wiggle_offset: int) -> list[float]:
+    wiggling = False  # I have some really silly variable names
+    # False means we're stabilizing, True means we're going horizontal
+    wiggle_countdown = wiggle_vertical
+    # do the offset
+    for _ in range(wiggle_offset):
+        wiggle_countdown -= 1
+        if wiggle_countdown <= 0:
+            wiggling = not wiggling
+            if wiggling:
+                wiggle_countdown = wiggle_horizontal
+            else:
+                wiggle_countdown = wiggle_vertical
+
+    frame_data = []
+    c_angle, c_speed = initial_angle, initial_speed
+    for f in range(frames):
+        angle_hold = 0
+        if wiggling:
+            angle_hold = 90.0 if facing == Facings.Right else 270.0
+        else:
+            angle_hold = optimizer.find_best_vertical_input(
+                c_angle, c_speed, facing)
+
+        frame_data.append(angle_hold)
+        c_angle, c_speed = simulator.simulate(c_angle, c_speed, angle_hold)
+
+        wiggle_countdown -= 1
+        if wiggle_countdown == 0:
+            wiggling = not wiggling
+            if wiggling:
+                wiggle_countdown = wiggle_horizontal
+            else:
+                wiggle_countdown = wiggle_vertical
+
+    return frame_data
+
 
 class Glideline:
     def __init__(self, main=None):
@@ -46,7 +74,7 @@ class Glideline:
         builder.add_from_file("./main.ui")
         self.mainwindow = builder.get_object('mainWindow', main)
         builder.connect_callbacks(self)
-    
+
         # builder.create_variable("wiggle_horizontal", "int")
         # builder.create_variable("wiggle_vertical", "int")
 
@@ -54,17 +82,17 @@ class Glideline:
 
     def default_setup(self):
         print("Running default window setup")
-        method:ttk.Combobox = self.builder.get_object('method')
+        method: ttk.Combobox = self.builder.get_object('method')
         method.current(0)
 
     def run(self):
         self.mainwindow.mainloop()
-    
+
     def optimize(self):
-        method:ttk.Combobox = self.builder.get_object('method')
+        method: ttk.Combobox = self.builder.get_object('method')
         active_method = method.current()
 
-        gamestate_box:tk.Entry = self.builder.get_object("gamestate")
+        gamestate_box: tk.Entry = self.builder.get_object("gamestate")
 
         frame_count = self.builder.get_variable("frames").get()
 
@@ -74,39 +102,49 @@ class Glideline:
         ]
 
         gamestate_data = gamestate_box.get("1.0", "end").rstrip().splitlines()
-        speedIndex = [1 if x.startswith("Speed") else 0 for x in gamestate_data].index(1)
+        speedIndex = [1 if x.startswith(
+            "Speed") else 0 for x in gamestate_data].index(1)
         speedString = gamestate_data[speedIndex][len("Speed: "):]
         speedX = float(speedString.split(", ")[0])
         speedY = float(speedString.split(", ")[1])
-        facing = Facings.Left if speedX < 0 else Facings.Right # flip the x speed if facing left
+        # flip the x speed if facing left
+        facing = Facings.Left if speedX < 0 else Facings.Right
 
         initial_speed = sqrt((speedX**2) + (speedY**2))
-        initial_angle = (((atan2(speedY, speedX * facing.value) * RAD_TO_DEG) + 90) + 360) % 360
+        initial_angle = (
+            ((atan2(speedY, speedX * facing.value) * RAD_TO_DEG) + 90) + 360) % 360
 
-        if active_method == 0: # normal pullup
-            frame_data = method_normal_pullup(initial_angle, initial_speed, facing, frame_count)
+        # get manual wiggle data
+        wiggle_horizontal = self.builder.get_variable("wiggle_horizontal").get()
+        wiggle_vertical = self.builder.get_variable("wiggle_vertical").get()
+        wiggle_offset = self.builder.get_variable("wiggle_offset").get()
+
+        if active_method == 0:  # normal pullup
+            frame_data = method_normal_pullup(
+                initial_angle, initial_speed, facing, frame_count)
             self.set_output_text(optimizer.frameDataToInputs(frame_data))
-        
-        elif active_method == 1: # megajoule method
+
+        elif active_method == 1:  # megajoule method
             self.set_output_text("Not yet implemented.")
-        
-        elif active_method == 2: # manual wiggle
-            self.set_output_text("Not yet implemented.")
+
+        elif active_method == 2:  # manual wiggle
+            frame_data = method_manual_wiggle(initial_angle, initial_speed, facing, frame_count, wiggle_horizontal, wiggle_vertical, wiggle_offset)
+            self.set_output_text(optimizer.frameDataToInputs(frame_data))
 
     def set_output_text(self, text):
-        output_box:tk.Entry = self.builder.get_object("output")
-
-        output_box.delete("1.0", "end")
+        output_box: tk.Entry = self.builder.get_object("output")
         output_box.config(state="normal")
+        output_box.delete("1.0", "end")
         output_box.insert("1.0", text)
         output_box.config(state="disabled")
 
     def copy_output(self):
-        output:tk.Text = self.builder.get_object("output")
+        output: tk.Text = self.builder.get_object("output")
         clipboard.copy(output.get("1.0", "end").rstrip())
-    
+
     def update_method(self, *args):
         print(args)
+
 
 if __name__ == "__main__":
     app = Glideline()
--- src/optimizer.py
+++ src/optimizer.py
@@ -4,21 +4,27 @@ from constants import *
 import simulator
 import time
 
-ELYTRA_HOTKEY = "PE" # change this if you're using PP or something else
-PRECISION_OUT = 4 # how many digits of precision are printed to the console
-PRECISION_COMPUTE = 12 # how many digits of precision are calculated (breaks down at 17 but you shouldn't need more than 12)
-HOLD_JUMP = False # this can allow you to get more height
+ELYTRA_HOTKEY = "PE"  # change this if you're using PP or something else
+PRECISION_OUT = 4  # how many digits of precision are printed to the console
+# how many digits of precision are calculated (breaks down at 17 but you shouldn't need more than 12)
+PRECISION_COMPUTE = 12
+HOLD_JUMP = False  # this can allow you to get more height
 JUMP_KEY = "J"
-STOP_VALUE = 0.001 # values below this number will NOT be added to output.
-DO_WIGGLE = False # whether or not to wiggle. You'll get an error if your frames below aren't setup right.
-WIGGLE_FRAMES_HORIZONTAL = 2 # these two variables control the wiggle. horizontal controls the amount of frames where we hold 90 / 270, and stabilizer is the amount of frames we hold up to change our direction.
-WIGGLE_FRAMES_STABILIZER = 2 # if DO_WIGGLE is true, neither of these should be 0.
+STOP_VALUE = 0.001  # values below this number will NOT be added to output.
+# whether or not to wiggle. You'll get an error if your frames below aren't setup right.
+DO_WIGGLE = False
+# these two variables control the wiggle. horizontal controls the amount of frames where we hold 90 / 270, and stabilizer is the amount of frames we hold up to change our direction.
+WIGGLE_FRAMES_HORIZONTAL = 2
+# if DO_WIGGLE is true, neither of these should be 0.
+WIGGLE_FRAMES_STABILIZER = 2
+
 
 def frange(min, max, step):
     value = min
     while value < max:
         value += step
         yield value
+
 
 def Approach(value, target, step):
     if value > target:
@@ -28,6 +34,8 @@ def Approach(value, target, step):
     return target
 
 # "santize" output (combine frames with repeated inputs)
+
+
 def sanitizer(output):
     new_out = [output.splitlines()[0]]
     for line in output.splitlines()[1:]:
@@ -36,11 +44,13 @@ def sanitizer(output):
         if angle == new_out[-1].split(",")[-1]:
             length = int(new_out[-1].split(",")[0].strip())
             length += 1
-            new_out[-1] = f"{length:4}{f',{JUMP_KEY}' if HOLD_JUMP else ''},{ELYTRA_HOTKEY},F,{angle}"
+            new_out[-1] = f"{length:4}{f',{JUMP_KEY}' if HOLD_JUMP else ''},{
+                ELYTRA_HOTKEY},F,{angle}"
         else:
             new_out.append(line)
-    
+
     return new_out
+
 
 def inputStringToFrameData(string) -> list[float]:
     frame_data = []
@@ -50,39 +60,41 @@ def inputStringToFrameData(string) -> list[float]:
     for t in t_data:
         for _ in range(t[0]):
             frame_data.append(t[1])
-    
+
     return frame_data
 
-def frameDataToInputs(frame_data, precision:int=4) -> str:
+
+def frameDataToInputs(frame_data, precision: int = 4) -> str:
     out = ""
     for f in frame_data:
         out += f"   1,PE,F,{f:.{precision}f}\n"
-    
+
     return "\n".join(sanitizer(out))
 
 
-def find_best_vertical_input(initial_angle:float, initial_speed:float, facing:Facings) -> float:
+def find_best_vertical_input(initial_angle: float, initial_speed: float, facing: Facings) -> float:
     """Finds the best input (in feather degrees) to maximize vertical velocity given the frame.
     - `initial_angle` : the initial flying angle in feather degrees.
     - `initial_speed` : the initial speed of the elytra.
     - `facing` the facing direction."""
 
     maxAngleChange = DELTA_TIME * MAX_ANGLE_CHANGE_INV_SPEED_FACTOR / initial_speed
-    
+
     angle_min = 0
     # if we're moving "down" but above the stable angle
     # then we don't want to optimize for immediate height
     if initial_angle > 90 and initial_angle < 90 + STABLE_ANGLE_DEG:
-        angle_min = (acos((STABLE_ANGLE - (((initial_angle - 90) * pi)/180.0))))*RAD_TO_DEG - (maxAngleChange * RAD_TO_DEG)
+        angle_min = (acos((STABLE_ANGLE - (((initial_angle - 90) * pi)/180.0)))
+                     )*RAD_TO_DEG - (maxAngleChange * RAD_TO_DEG)
     angle_max = 180
-    
+
     init_min_angle = angle_min
     init_max_angle = angle_max
 
-    best_yspeed = float("inf") # best speed gotten
+    best_yspeed = float("inf")  # best speed gotten
     best_total_speed = float("inf")
-    best_angleI = 0 # best angle INPUT
-    best_angleF = 0 # best angle FLIGHT
+    best_angleI = 0  # best angle INPUT
+    best_angleF = 0  # best angle FLIGHT
     step_size = 1
     iteration = 1
 
@@ -90,7 +102,8 @@ def find_best_vertical_input(initial_angle:float, initial_speed:float, facing:Fa
         for angle in frange(angle_min, angle_max, step_size):
             # simulate in-game speed changes
             new_speed = initial_speed
-            new_angle, new_speed = simulator.simulate(initial_angle, initial_speed, angle)
+            new_angle, new_speed = simulator.simulate(
+                initial_angle, initial_speed, angle)
             ySpeed = new_speed * -sin((90 - new_angle) * DEG_TO_RAD)
 
             # since y speed is negative when moving up
@@ -99,7 +112,7 @@ def find_best_vertical_input(initial_angle:float, initial_speed:float, facing:Fa
                 best_total_speed = new_speed
                 best_angleI = angle
                 best_angleF = new_angle
-        
+
         # once we've found our best angle for this frame, narrow down precision
         angle_min = best_angleI - 10/(10**iteration)
         angle_max = best_angleI + 10/(10**iteration)
@@ -119,6 +132,6 @@ def find_best_vertical_input(initial_angle:float, initial_speed:float, facing:Fa
 
     angle_hold = best_angleI
     if facing == Facings.Left:
-        angle_hold = ((-best_angleI)%360)
-    
+        angle_hold = ((-best_angleI) % 360)
+
     return angle_hold
