4491d4702bffc31d336690ed1ab2dc9d10fc723f
megamaz <raphael.mazuel@gmail.com>
2025-01-24 16:35:16 -0800

    fixed venv issues with batch

--- run.bat
+++ run.bat
@@ -4,9 +4,16 @@ cd src
 if not exist venv (
     echo Creating venv
     py -m venv venv
+    echo Activating venv
+    call ".\venv\Scripts\activate.bat"
     echo Installing requirements
-    .\venv\Scripts\python.exe -m pip install -r requirements.txt
+    python -m pip install -r requirements.txt
 )
+@REM Works because we can't activate an already activated venv
+@REM So this does nothing if we *just* went through venv setup
+call ".\venv\Scripts\activate.bat"
 
 echo Running
-.\venv\Scripts\python.exe main.py
+python main.py
+
+call "deactivate.bat"
